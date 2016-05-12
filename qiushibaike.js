4205982e7b609ac33210939ca1b79d7cad7b6d0b
ShenJianShou <admin@bihu.mobi>
2016-05-12 11:16:06 +0800

    Create qiushibaike.js

---
+++ qiushibaike.js
@@ -0,0 +1,17 @@
+var configs = {
+    domains: ["www.qiushibaike.com"],
+    scanUrls: ["http://www.qiushibaike.com/"],
+    contentUrlRegexes: ["http://www\\.qiushibaike\\.com/article/\\d+"],
+    fields: [
+        {
+            name: "content",
+            selector: "//*[@id='single-next-link']",
+            required: true
+        },
+        {
+            name: "author",
+            selector: "//div[contains(@class,'author')]//h2"
+        }
+    ]
+};
+start(configs);
