ac813f7360d63e2e2c0530c728ec1678eab05c7b
ShenJianShou <admin@bihu.mobi>
2016-06-07 16:34:14 +0800

    更新大众点评网的爬虫源码

--- dianping.js
+++ dianping.js
@@ -1,4 +1,7 @@
-// 大众点评上爬取所有"黄焖鸡米饭"的商户信息
+/*使用javascript编写的爬虫源码，用于爬取大众点评上所有"黄焖鸡米饭"的商户信息。
+代码粘贴到神箭手云爬虫平台（http://www.shenjianshou.cn/）上就可以直接跑了，不需要安装编译环境。要爬取其他网站，可以更改源码即可。
+代码执行具体步骤请参考：https://github.com/ShenJianShou/crawler_samples/blob/master/%E5%A6%82%E4%BD%95%E6%89%A7%E8%A1%8C%E6%A0%B7%E4%BE%8B%E4%BB%A3%E7%A0%81.txt
+*/
 var keywords = "黄焖鸡米饭";
 var scanUrls = ["http://www.dianping.com/search/keyword/1/0_"+keywords];
 
