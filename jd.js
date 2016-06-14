37e831a298a9b8d7153e41ebae1c446a3c89bfa7
ShenJianShou <admin@bihu.mobi>
2016-06-14 10:37:30 +0800

    更新京东商品信息和评价采集爬虫

--- jd.js
+++ jd.js
@@ -1,6 +1,5 @@
 /*使用javascript编写的爬虫源码，用于爬取京东商城上的商品信息和评论。
 代码粘贴到神箭手云爬虫平台（http://www.shenjianshou.cn/）上就可以直接跑了，不需要安装编译环境。要爬取其他网站，可以更改源码即可。
-
 代码执行具体步骤请参考：https://github.com/ShenJianShou/crawler_samples/blob/master/%E5%A6%82%E4%BD%95%E6%89%A7%E8%A1%8C%E6%A0%B7%E4%BE%8B%E4%BB%A3%E7%A0%81.txt
 */
 
@@ -10,15 +9,16 @@ var comment_count = 100;//@input(comment_count, 爬取的评论数, 最多爬取
 var page_count = comment_count / 10;
 keyword = keyword.trim();
 var scanUrls = [];
-scanUrls.push("http://search.jd.com/Search?keyword="+keyword.replace(/ /g, "+")+"&enc=utf-8");
+scanUrls.push("http://search.jd.com/Search?keyword=" + keyword.replace(/ /g, "+") + "&enc=utf-8&scrolling=y&page=200");
 var helperUrlRegexes = [];
-helperUrlRegexes.push("http://search\\.jd\\.com/Search\\?keyword="+keyword.replace(/ /g, "\\+").replace(/\./g, "\\.")+"&enc=utf-8");
+helperUrlRegexes.push("http://search\\.jd\\.com/Search\\?keyword=" + keyword.replace(/ /g, "\\+").replace(/\./g, "\\.") + "&enc=utf-8&scrolling=y&page=\\d+");
 
 var configs = {
-    domains: ["search.jd.com","item.jd.com","club.jd.com"],
+    domains: ["search.jd.com", "item.jd.com", "club.jd.com"],
     scanUrls: scanUrls,
     contentUrlRegexes: ["http://item\\.jd\\.com/\\d+.html"],
-    helperUrlRegexes: helperUrlRegexes, 
+    helperUrlRegexes: helperUrlRegexes,
+    interval: 10000,
     fields: [
         {
             // 第一个抽取项
@@ -65,6 +65,7 @@ var configs = {
         }
     ]
 };
+
 configs.afterDownloadPage = function(page, site) {
     var matches = /item\.jd\.com\/(\d+)\.html/.exec(page.url);
     if (!matches) return page;
@@ -83,18 +84,28 @@ configs.afterDownloadPage = function(page, site) {
     page.raw = page.raw.substring(0, index) + pageHtml + page.raw.substring(index);
     return page;
 };
-configs.onProcessHelperUrl = function(url, content, site){
-    if(!content.indexOf("抱歉，没有找到")){
-        var currentPage = parseInt(url.substring(url.indexOf("&page=") + 6));
-        if(currentPage === 0){
-            currentPage = 1;
-        }
-        var page = currentPage + 2;
-        var nextUrl = url.replace("&page=" + currentPage, "&page=" + page);
-        site.addUrl(nextUrl);
+
+var dataSku = 0;
+
+configs.onProcessHelperPage = function(page, content, site) {
+    var num = parseInt(extract(content, "//*[@id='J_goodsList']/ul/li[1]/@data-sku"));
+    if (dataSku === 0) {
+        dataSku = isNaN(num) ? 0 : num;
     }
+    else if (dataSku === num) {
+        dataSku = 0;
+        return false;
+    }
+    var currentPageNum = parseInt(page.url.substring(page.url.indexOf("&page=") + 6));
+    if (currentPageNum === 0) {
+        currentPageNum = 1;
+    }
+    var pageNum = currentPageNum + 1;
+    var nextUrl = page.url.replace("&page=" + currentPageNum, "&page=" + pageNum);
+    site.addUrl(nextUrl);
     return true;
 };
+
 configs.afterExtractPage = function(page, data) {
     if (data.comments === null || data.comments === undefined) return data;
     var comments = [];
@@ -107,5 +118,6 @@ configs.afterExtractPage = function(page, data) {
     data.comments = comments;
     return data;
 };
+
 var crawler = new Crawler(configs);
 crawler.start();
