b33956e3617bd7ec03964240eb6cd3bf09abbadb
ShenJianShou <admin@bihu.mobi>
2016-07-20 12:26:22 +0800

    修改今日头条爬虫源码

--- jinritoutiao.js
+++ jinritoutiao.js
@@ -1,4 +1,4 @@
-/*使用javascript编写的爬虫源码，用于爬取今日头条的新闻。
+/*使用javascript编写的爬虫源码（神箭手开发者投稿作品），用于爬取今日头条的新闻。
 代码粘贴到神箭手云爬虫平台（http://www.shenjianshou.cn/）上就可以直接跑了，不需要安装编译环境。要爬取其他网站，可以更改源码即可。
 代码执行具体步骤请参考：https://github.com/ShenJianShou/crawler_samples/blob/master/%E5%A6%82%E4%BD%95%E6%89%A7%E8%A1%8C%E6%A0%B7%E4%BE%8B%E4%BB%A3%E7%A0%81.txt
 */
